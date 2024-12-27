d2214c602731b63dcb302eb30ffac4aa8e2ecd45
prasanthLalapeta <sonoffrontend.developer@gmail.com>
2024-12-27 17:40:51 +0530

    chore: updated upload API request

--- app/api/upload.ts
+++ app/api/upload.ts
@@ -9,9 +9,6 @@ export const config = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed' });
-    }
 
     try {
         if (!req.url) {
